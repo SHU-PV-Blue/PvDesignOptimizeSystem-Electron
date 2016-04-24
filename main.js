9cff5e4b710b309dd5b85cef036a20d8e4f37cba
dayang <1306693844@qq.com>
2016-04-24 11:46:09 +0800

    基本流程走通

--- main.js
+++ main.js
@@ -3,8 +3,125 @@ const electron = require('electron');
 const app = electron.app;
 const BrowserWindow = require('browser-window');
 const ipcMain = require('electron').ipcMain;
+const nativeImage = require('electron').nativeImage;
+const Menu = require('electron').Menu;
+var image = nativeImage.createFromPath('res/images/tray.png');
 
 var mainWindow = null;
+
+var template = [
+  {
+  	label: '文件',
+  	submenu: [
+  		{
+  			label : '创建新项目',
+  			accelerator : 'Ctrl+N'
+  		},
+  		{
+  			label : '打开项目',
+  			accelerator : 'Ctrl+O'
+  		},
+  		{
+  			type: 'separator'
+  		},
+  		{
+  			label : '退出',
+  			accelerator : 'Ctrl+E'
+  		}
+  	]
+  },
+  {
+    label: '编辑',
+    submenu: [
+      {
+        label: '撤销',
+        accelerator: 'CmdOrCtrl+Z',
+        role: 'undo'
+      },
+      {
+        label: '重做',
+        accelerator: 'Shift+CmdOrCtrl+Z',
+        role: 'redo'
+      },
+      {
+        type: 'separator'
+      },
+      {
+        label: '剪切',
+        accelerator: 'CmdOrCtrl+X',
+        role: 'cut'
+      },
+      {
+        label: '复制',
+        accelerator: 'CmdOrCtrl+C',
+        role: 'copy'
+      },
+      {
+        label: '粘贴',
+        accelerator: 'CmdOrCtrl+V',
+        role: 'paste'
+      }
+    ]
+  },
+  {
+    label: '视图',
+    submenu: [
+      {
+        label: '重新加载',
+        accelerator: 'CmdOrCtrl+R',
+        click: function(item, focusedWindow) {
+          if (focusedWindow)
+            focusedWindow.reload();
+        }
+      },
+      {
+        label: '切换全屏',
+        accelerator: (function() {
+          if (process.platform == 'darwin')
+            return 'Ctrl+Command+F';
+          else
+            return 'F11';
+        })(),
+        click: function(item, focusedWindow) {
+          if (focusedWindow)
+            focusedWindow.setFullScreen(!focusedWindow.isFullScreen());
+        }
+      },
+      {
+        label: '开发者工具',
+        accelerator: (function() {
+          if (process.platform == 'darwin')
+            return 'Alt+Command+I';
+          else
+            return 'Ctrl+Shift+I';
+        })(),
+        click: function(item, focusedWindow) {
+          if (focusedWindow)
+            focusedWindow.webContents.toggleDevTools();
+        }
+      },
+    ]
+  },
+  {
+    label: '窗口',
+    role: 'window',
+    submenu: [
+      {
+        label: '最小化',
+        accelerator: 'CmdOrCtrl+M',
+        role: 'minimize'
+      },
+      {
+        label: '关闭',
+        accelerator: 'CmdOrCtrl+W',
+        role: 'close'
+      }
+    ]
+  }
+];
+
+var menu = Menu.buildFromTemplate(template);
+Menu.setApplicationMenu(menu);
 
 app.on('window-all-closed',function(){
 	if(process.platform != 'darwin'){
@@ -15,7 +132,9 @@ app.on('window-all-closed',function(){
 app.on('ready',function(){
 	mainWindow = new BrowserWindow({
 		width : 1200,
-		height : 800
+		height : 800,
+		title : 'PvDesignOptimizeSystem',
+		icon : image
 	});
 	mainWindow.loadURL('file://' + __dirname + '/index.html');
 
--- static/javascript/index.js
+++ static/javascript/index.js
@@ -1,17 +1,36 @@
-var pvModule = angular.module('PVModule',['ui.bootstrap']);
+var pvModule = angular.module('PVModule',['ui.bootstrap','ui.router','ngRoute']);
+pvModule.config(function($routeProvider){
+	$routeProvider.when('/', {
+        templateUrl: 'tpls/test1.html'
+    }).when('/test2',{
+    	templateUrl: 'tpls/test2.html'
+    }).when('/test3',{
+    	templateUrl: 'tpls/test3.html'
+    }).when('/test4',{
+    	templateUrl: 'tpls/test4.html'
+    }).when('/test5',{
+    	templateUrl: 'tpls/test5.html'
+    });
+    $routeProvider.otherwise({ redirectTo: '/' });
+})
+
 pvModule.controller('cordionCtrl',function($scope){
 	 $scope.oneAtATime = true;
+});
 
-	  $scope.groups = [
-	    {
-	      title: 'Dynamic Group Header - 1',
-	      content: 'Dynamic Group Body - 1'
-	    },
-	    {
-	      title: 'Dynamic Group Header - 2',
-	      content: 'Dynamic Group Body - 2'
-	    }
-	  ];
-
-	  $scope.items = ['Item 1', 'Item 2', 'Item 3'];
-})
+pvModule.controller('prenextCtrl',function($scope, $location){
+	$scope.routes = ['/','/test2','/test3','/test4','/test5']
+	$scope.curIndex = 0;
+	$scope.nextStep = function(){
+		if($scope.curIndex >= $scope.routes.length - 1)
+			return;
+		$scope.curIndex +=1;
+		$location.path($scope.routes[$scope.curIndex]);
+	};
+	$scope.preStep = function(target){
+		if($scope.curIndex == 0)
+			return;
+		$scope.curIndex -=1;
+		$location.path($scope.routes[$scope.curIndex]);
+	}
+});
