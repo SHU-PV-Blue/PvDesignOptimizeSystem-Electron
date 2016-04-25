f938a9e4f02f74d5379df3873ce0b547e14efc5e
SnackMan <vipkia@sina.cn>
2016-04-25 17:45:18 +0800

    更新两张图片

--- tpls/javascript/radioButton.js
+++ tpls/javascript/radioButton.js
@@ -11,9 +11,19 @@ $(document).ready(function() {
         $("#user-defined").css("display","block");
         $("#give-area-component").css("display","none");
     });
+    //改变radio样式
     $('.iCheck').iCheck({
         checkboxClass: 'icheckbox_flat-blue',
         radioClass: 'iradio_flat-blue'
     });
-
+    //显示集中式四张图
+    $("#centralism-inverter").on('ifChecked', function(){
+        $("#centralism-instrument").css("display","block");
+        $("#group-instrument").css("display","none");
+    });
+    //显示组串式三张图
+    $("#group-inverter").on('ifChecked', function(){
+        $("#group-instrument").css("display","block");
+        $("#centralism-instrument").css("display","none");
+    });
 });
