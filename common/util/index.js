2f53f4f7872bc326d6fe3ba256b70bc6c7ec212d
dayang <1306693844@qq.com>
2016-08-06 23:23:42 +0800

    增加util工具文件

---
+++ common/util/index.js
@@ -0,0 +1,21 @@
+exports.CONST = {
+    monthDays : [31, 28, 31, 30, 31, 30, 31, 31 ,30, 31, 30, 31]
+};
+
+exports.Functions = {
+    getLabel : function(max){
+        var label = [],i;
+        for(i = 1; i <= max; i++){
+            label.push(i);
+        }
+        return label;
+    },
+    getDipLabel : function(){
+        var label = [],i;
+        for(i = 0; i<= 90; i++){
+            label.push((i % 5 === 0 ? i + '' : ''));
+        }
+        return label;
+    }
+};
+
